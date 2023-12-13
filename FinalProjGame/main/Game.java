49a3e29ccca3ed962c86ee2e84737a2079892285
Stanleigh Morales <stanleighmorales@gmail.com>
2023-12-14 02:31:34 +0800

    Add files via upload
    For the 2D platform based game, for Final exam

---
+++ FinalProjGame/main/Game.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Game {
+    private GameWindow gamewindow;
+    private GamePanel gamepanel;
+    public Game() {
+
+
+        gamepanel = new GamePanel();
+        gamewindow = new GameWindow(gamepanel);
+        gamepanel.requestFocus();
+
+
+    }
+}
